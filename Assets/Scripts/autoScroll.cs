d6aeb2f7906c75949804bd7f029c99fc3aa4e05a
Flavio Zamperlini <flavio.zamperlini@universite-paris-saclay.fr>
2025-01-24 14:22:44 +0100

    AutoScroll

--- Assets/Scripts/autoScroll.cs
+++ Assets/Scripts/autoScroll.cs
@@ -4,19 +4,14 @@ using UnityEngine;
 
 public class autoScroll : MonoBehaviour
 {
-    public float scrollSpeed = 5f; // Vitesse de défilement
-    public float resetPosition = 20f; // Position où le fond revient au départ
-    public float startPosition = 0f; // Position initiale du fond
+    public float speed;
+    Vector2 offset;
+    
 
-    void Update()
+    // Update is called once per frame
+    private void Update()
     {
-        // Déplacement vers la gauche
-        transform.Translate(Vector3.left * scrollSpeed * Time.deltaTime);
-
-        // Réinitialisation de la position quand l'objet sort de l'écran
-        if (transform.position.x <= -resetPosition)
-        {
-            transform.position = new Vector3(startPosition, transform.position.y, transform.position.z);
-        }
+        offset = new Vector2(0,Time.time * speed) ;
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
     }
 }
