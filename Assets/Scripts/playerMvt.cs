9fa0691448a4b7b12c4932e7c01d5166bf44d676
Mathis Heder <mathis.heder@universite-paris-saclay.fr>
2025-01-24 12:06:23 +0100

    player mvt et scene nulle

---
+++ Assets/Scripts/playerMvt.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    Rigidbody2D rb;
+    [SerializeField] float speed;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal"); // recuperer quand le joueur appui sur les touches gauche et droite
+        
+        if (x < 0f )
+        {
+            rb.velocity = new Vector2(rb.velocity.x - speed, rb.velocity.y); // assigner la position du joueur 
+        }
+        else if(x > 0f )
+        {
+            rb.velocity = new Vector2(rb.velocity.x + speed, rb.velocity.y );// assigner la position du joueur 
+        }
+    }
+}
